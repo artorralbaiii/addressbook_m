19eb50d03bcaa8d78ad87e4f7c831c17cb91bb8b
Andres Jr III R Torralba <a.r.torralba.iii@gmail.com>
2016-12-07 13:04:59 +0800

    Connect to MongoDB Compose

--- src/server/app.config.js
+++ src/server/app.config.js
@@ -4,7 +4,8 @@ module.exports = function () {
     var config = {
         /* Database Config */
         //db: 'mongodb://admin:password@localhost:27017/addressbook' // local DB
-        db: 'mongodb://admin:passw0rd@ds159497.mlab.com:59497/addressbook' // Mongo Lab
+        //db: 'mongodb://admin:passw0rd@ds159497.mlab.com:59497/addressbook' // Mongo Lab
+        db: 'mongodb://admin:AHUCQSZXZXVVTWDL@sl-us-dal-9-portal.4.dblayer.com:17447,sl-us-dal-9-portal.3.dblayer.com:17447/admin?ssl=true'
     };
 
     return config;
--- src/server/app.js
+++ src/server/app.js
@@ -7,15 +7,30 @@ var logger = require('morgan');
 var compress = require('compression');
 var favicon = require('serve-favicon');
 var mongoose = require('mongoose');
+var cfenv = require('cfenv');
 
 var config = require('./app.config')();
 var port = process.env.PORT || 3000;
 var environment = process.env.NODE_ENV;
 var errorHandler = require('./routes/utils/errorHandler')();
 var app = express();
+var appenv = cfenv.getAppEnv();
+var services = appenv.services;
+var mongodb_services = services["compose-for-mongodb"];
+var credentials = mongodb_services[0].credentials;
+
+var ca = [new Buffer(credentials.ca_certificate_base64, 'base64')];
 
 // Database Connection
-mongoose.connect(config.db, function (err) {
+mongoose.connect(credentials.uri,{
+    mongos: {
+        ssl: true,
+        sslValidate: true,
+        sslCA: ca,
+        poolSize: 1,
+        reconnectTries: 1
+    }
+}, function (err) {
     if (err) {
         console.log(err);
     } else {
